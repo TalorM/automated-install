8a06c5de5e83e4f29bcba2e07e359b0c534d4db5
Talor M <110429857+TalorM@users.noreply.github.com>
2024-10-22 14:43:30 -0600

    Update Workstation Software Installations.bat

--- Workstation Software Installations.bat
+++ Workstation Software Installations.bat
@@ -1,7 +1,5 @@
-@echo off
-
 echo Making directory at C:\WorkstationSetup\Auto
-mkdir C:\WorkstationSetup\Auto
+mkdir C:\WorkstationSetup\Auto 2>nul
 cd C:\WorkstationSetup\Auto
 
 REM Define file variables
@@ -23,34 +21,24 @@ set FILE8=Python.exe
 set URL8=https://github.com/TalorM/automated-install/releases/tag/v1.0/%FILE8%
 
 echo Downloading install files from GitHub
-curl -L -o "%FILE1%" %URL1%
-curl -L -o "%FILE2%" %URL2%
-curl -L -o "%FILE3%" %URL3%
-curl -L -o "%FILE4%" %URL4%
-curl -L -o "%FILE5%" %URL5%
-curl -L -o "%FILE6%" %URL6%
-curl -L -o "%FILE7%" %URL7%
-curl -L -o "%FILE8%" %URL8%
-
-REM Function to install an application with error handling (Praying this works)...
-:install_app
-%1
-if errorlevel 1 (
-    echo Failed to install %1
-) else (
-    echo %1 installed successfully.
-)
-goto :eof
+curl -L -o "%FILE1%" %URL1% || (echo Failed to download %FILE1% & exit /b)
+curl -L -o "%FILE2%" %URL2% || (echo Failed to download %FILE2% & exit /b)
+curl -L -o "%FILE3%" %URL3% || (echo Failed to download %FILE3% & exit /b)
+curl -L -o "%FILE4%" %URL4% || (echo Failed to download %FILE4% & exit /b)
+curl -L -o "%FILE5%" %URL5% || (echo Failed to download %FILE5% & exit /b)
+curl -L -o "%FILE6%" %URL6% || (echo Failed to download %FILE6% & exit /b)
+curl -L -o "%FILE7%" %URL7% || (echo Failed to download %FILE7% & exit /b)
+curl -L -o "%FILE8%" %URL8% || (echo Failed to download %FILE8% & exit /b)
 
 echo Installing applications
-call :install_app "msiexec /i %FILE1% /quiet /norestart"
-call :install_app "start /wait %FILE2% /s"
-call :install_app "msiexec /i %FILE3% /quiet /norestart"
-call :install_app "start /wait %FILE4% /s"
-call :install_app "start /wait %FILE5% /s"
-call :install_app "start /wait %FILE6% /s"
-call :install_app "start /wait %FILE7% /s"
-call :install_app "start /wait %FILE8% /s"
+msiexec /i "%FILE1%" /quiet /norestart || (echo Failed to install %FILE1% & exit /b)
+start /wait "%FILE2%" /s || (echo Failed to install %FILE2% & exit /b)
+msiexec /i "%FILE3%" /quiet /norestart || (echo Failed to install %FILE3% & exit /b)
+start /wait "%FILE4%" /s || (echo Failed to install %FILE4% & exit /b)
+start /wait "%FILE5%" /s || (echo Failed to install %FILE5% & exit /b)
+start /wait "%FILE6%" /s || (echo Failed to install %FILE6% & exit /b)
+start /wait "%FILE7%" /s || (echo Failed to install %FILE7% & exit /b)
+start /wait "%FILE8%" /s || (echo Failed to install %FILE8% & exit /b)
 
 echo Installation process completed.
 pause
